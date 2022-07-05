aa48a348857b68ea6206c7639798f924f132f8ad
Beksultan <96925396+Vazno@users.noreply.github.com>
2022-07-05 11:46:19 +0600

    Rewrote main function to class

--- generate_nick.py
+++ generate_nick.py
@@ -4,65 +4,93 @@ import json
 from urllib.request import urlopen
 from typing import List
 
-# On Windows, calling init() will filter ANSI escape sequences out of any text sent to stdout or stderr, and replace 
-# them with equivalent Win32 calls.
-colorama.init(True)
+class Generate_minecraft_nick():
+	def __init__(self, length, attempts) -> List:
+		'''Generate O.G minecraft nicknames'''
+		# On Windows, calling init() will filter ANSI escape sequences out of any text sent to stdout or stderr, and replace 
+		# them with equivalent Win32 calls.
+		colorama.init(True)
 
-# Generating json file, to cache nicks
-try:
-	cache_nicks = json.load(open("cache_nicks.json"))
-except FileNotFoundError:
-	example = {"Example": False}
-	json.dump(example, open("cache_nicks.json", "w"))
-	cache_nicks = json.load(open("cache_nicks.json", "r"))
+		self.__raise_errors(length)
 
-def generate_minecraft_nick(length=4, attempts = 600) -> List:
+		self.cache_nicks = self.config_json("cache_nicks.json")
+		self.name_list = self.generate_nicks(attempts=attempts, length=length, check_json=self.cache_nicks)
 
-	if length > 16:
-		raise ValueError("The length of the nickname can't be bigger than 16 characters")
-	if length <= 2:
-		raise ValueError("The length of the nickname can't be less than 2 characters")
-
-	valid_list = []
-	name_list = []
-
-	# Generating nicknames
-	for i in range(attempts):
-		name = stringtools.generate_nick(length=length)
-		# Checking if nickname is already cached
-		if name in cache_nicks:
-			print(f"Name: {name}, was already checked, and it's", cache_nicks[name])
-		else:
-			name_list.append(name)
+		self.attempts = attempts
+		self.length = length
+		self.response = self.request_api(self.name_list)
 
 
-	for name in name_list:
-		# Storing the URL in url as parameter for urlopen
+	def __str__(self) -> str:
+		return str(self.response)
 
-		url = f"https://api.mojang.com/users/profiles/minecraft/{name}"
+	def __iter__(self) -> List:
+		return iter(self.response)
 
-		# Storing the response of URL
-		response = urlopen(url)
 
-		# Trying to store the JSON response
+	def generate_nicks(self, attempts, length, check_json):
+		name_list = [] # Storing nicknames here
+
+		# Generating nicknames using stringtools
+		for i in range(attempts):
+			name = stringtools.generate_nick(length=length)
+
+			# Checking if nickname is already cached
+			if name in self.cache_nicks:
+				print(f"Name: {name}, was already checked, and it's", check_json[name])
+			# If not, adding to name_list
+			else:
+				name_list.append(name)
+		return name_list
+
+	def config_json(self, json_name):
+		'''Generate json file, to cache nicks'''
 		try:
+			cache_nicks = json.load(open(json_name))
+		except FileNotFoundError:
+			example = {"Example": False}
+			json.dump(example, open(json_name, "w"))
+			cache_nicks = json.load(open(json_name, "r"))
+		finally:
+			return cache_nicks
 
-			data_json = json.loads(response.read())
-			print(f"{colorama.Fore.LIGHTBLACK_EX + 'Name:'} {colorama.Fore.RED + name}")
+	def __raise_errors(self, length):
+		if length > 16:
+			raise ValueError("The length of the nickname can't be bigger than 16 characters")
+		elif length <= 2:
+			raise ValueError("The length of the nickname can't be less than 2 characters")
+	
+	def __write_to_json(self, name, _bool):
+		self.cache_nicks[name] = _bool
+		json.dump(self.cache_nicks, open("cache_nicks.json", "w+"), indent=4, sort_keys=True)
 
-			# Caching the nickname
-			cache_nicks[name] = False
-			json.dump(cache_nicks, open("cache_nicks.json", "w+"), indent=4, sort_keys=True)
+	def request_api(self, names: List) -> List:
+		'''Returns available nicknames.'''
+		# Requesting information from https://api.mojang.com/users/profiles/minecraft/name
+
+		valid_list = [] # Storing available nicknames in here
 		
-		# If there is nothing to store, than the nickname is available
-		except json.JSONDecodeError:
-			valid_list.append(name)
-			print(f"{colorama.Fore.LIGHTWHITE_EX + 'Name:'} {colorama.Fore.GREEN + name}")
+		for name in names:
+			# Storing the URL in url as parameter for urlopen
+			url = f"https://api.mojang.com/users/profiles/minecraft/{name}"
 
-			# Caching the nickname
-			cache_nicks[name] = True
-			json.dump(cache_nicks, open("cache_nicks.json", "w+"), indent=4, sort_keys=True)
+			# Storing the response of URL
+			response = urlopen(url)
 
-	return valid_list
+			# Trying to store the JSON response
+			try:
+				data_json = json.loads(response.read())
+				print(f"{colorama.Fore.LIGHTBLACK_EX + 'Name:'} {colorama.Fore.RED + name}")
 
-print(" ".join(generate_minecraft_nick(length=4, attempts=50)))
+				# Caching the nickname
+				self.__write_to_json(name, False)
+			
+			# If there is nothing to store, than the nickname is available
+			except json.JSONDecodeError:
+				valid_list.append(name)
+				print(f"{colorama.Fore.LIGHTWHITE_EX + 'Name:'} {colorama.Fore.GREEN + name}")
+
+				# Caching the nickname
+				self.__write_to_json(name, True)
+
+		return valid_list
